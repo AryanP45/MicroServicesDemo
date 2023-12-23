8cf8c8947d653a4179e07f9ae660343a92679fe7
Aryan <aryanitinpatil@gmail.com>
2023-12-23 15:06:05 +0530

    exception handled on getAllQuestion endpoint
    return responseEntiity with http codes

--- src/main/java/com/aryan/quiz/controllers/QuestionController.java
+++ src/main/java/com/aryan/quiz/controllers/QuestionController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +27,7 @@ public class QuestionController {
 	QuestionService questionService;
 
 	@GetMapping("/allquestions")
-	public List<Question> getAllQuestion() {
+	public ResponseEntity<List<Question>> getAllQuestion() {
 		return questionService.getAllQuestions();
 	}
 
--- src/main/java/com/aryan/quiz/services/QuestionService.java
+++ src/main/java/com/aryan/quiz/services/QuestionService.java
@@ -1,9 +1,12 @@
 package com.aryan.quiz.services;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.aryan.quiz.dao.QuestionDao;
@@ -15,8 +18,13 @@ public class QuestionService {
 	@Autowired
 	QuestionDao questionDao;
 
-	public List<Question> getAllQuestions() {
-		return questionDao.findAll();
+	public ResponseEntity<List<Question>> getAllQuestions() {
+		try {
+			return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>(new ArrayList<>(), HttpStatus.NOT_FOUND);
 	}
 
 	public List<Question> getQuestionsByCategory(String name) {
