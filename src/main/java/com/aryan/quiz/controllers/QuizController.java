67b047ca3603a5734ebe422472c8268d826cef17
Aryan <aryanitinpatil@gmail.com>
2023-12-23 18:27:38 +0530

    Created Quiz Create Endpoint
     Endpoint + Exception Handled + httpStatusCode modified

--- src/main/java/com/aryan/quiz/controllers/QuizController.java
+++ src/main/java/com/aryan/quiz/controllers/QuizController.java
@@ -1,24 +1,32 @@
 package com.aryan.quiz.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.aryan.quiz.models.Quiz;
+import com.aryan.quiz.services.QuizService;
+
 @RestController
 @RequestMapping("/quiz")
 public class QuizController {
 
+	@Autowired
+	QuizService quizService;
+
 	@PostMapping("/create")
-	public ResponseEntity<String> createQuiz(@RequestParam String category,@RequestParam String noOfQuestion,@RequestParam String title){
-		
-		return new ResponseEntity<>("Quiz Created"
-				+ "Category : "+category
-				+ "Number of Question : "+Integer.parseInt(noOfQuestion)
-				+ "Title : "+title,HttpStatus.OK);
-		
+	public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam String noOfQuestion,
+			@RequestParam String title) {
+		return quizService.createQuiz(category, noOfQuestion, title);
+
+	}
 	}
 }
--- src/main/java/com/aryan/quiz/services/QuizService.java
+++ src/main/java/com/aryan/quiz/services/QuizService.java
@@ -1,14 +1,41 @@
 package com.aryan.quiz.services;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.aryan.quiz.dao.QuestionDao;
 import com.aryan.quiz.dao.QuizDao;
+import com.aryan.quiz.models.Question;
+import com.aryan.quiz.models.Quiz;
 
 @Service
 public class QuizService {
 	@Autowired
 	QuizDao quizDao;
-	
-	
+
+	@Autowired
+	QuestionDao questionDao;
+
+	public ResponseEntity<String> createQuiz(String category, String noOfQuestion, String title) {
+		try {
+			List<Question> questions = questionDao.findRandomQuestionsByCategory(category,
+					Integer.parseInt(noOfQuestion));
+			Quiz quiz = new Quiz();
+			quiz.setTitle(title);
+			quiz.setQuestions(questions);
+			quizDao.save(quiz);
+			return new ResponseEntity<>(quiz.toString() + " Success", HttpStatus.CREATED);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>("Quiz cannot be created please check parameters correctly",
+				HttpStatus.NOT_ACCEPTABLE);
+	}
 }
