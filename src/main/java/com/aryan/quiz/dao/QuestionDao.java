ac649e3cc08a6fd621481f0090e3ff461bc27cd7
Aryan <aryanitinpatil@gmail.com>
2023-12-23 18:25:30 +0530

    Query for findRandomQuestionsByCategory with LIMIT
    This will be used by QuizService to get Questions based on category and numberOfQuestions

--- src/main/java/com/aryan/quiz/dao/QuestionDao.java
+++ src/main/java/com/aryan/quiz/dao/QuestionDao.java
@@ -1,13 +1,19 @@
 package com.aryan.quiz.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.aryan.quiz.models.Question;
+
 import java.util.List;
 
 
 @Repository
 public interface QuestionDao extends JpaRepository<Question, Integer>{
 	List<Question> findByCategory(String category);
+
+	@Query(value = "select * from question q Where q.category =:category ORDER BY RANDOM() LIMIT :noOfQuestion",nativeQuery=true)
+	List<Question> findRandomQuestionsByCategory(String category, Integer noOfQuestion);
+
 }
