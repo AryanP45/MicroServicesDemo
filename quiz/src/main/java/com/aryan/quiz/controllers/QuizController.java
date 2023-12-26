d2671ce2295e5353f00831ebf2582504600e8303
Aryan <aryanitinpatil@gmail.com>
2023-12-26 14:34:25 +0530

    rm unused import

--- quiz/src/main/java/com/aryan/quiz/controllers/QuizController.java
+++ quiz/src/main/java/com/aryan/quiz/controllers/QuizController.java
@@ -3,7 +3,6 @@ package com.aryan.quiz.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.aryan.quiz.models.Question;
 import com.aryan.quiz.models.QuestionWrapper;
 import com.aryan.quiz.models.Quiz;
 import com.aryan.quiz.models.Response;
--- quiz/src/main/java/com/aryan/quiz/services/QuizService.java
+++ quiz/src/main/java/com/aryan/quiz/services/QuizService.java
@@ -2,11 +2,8 @@ package com.aryan.quiz.services;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
