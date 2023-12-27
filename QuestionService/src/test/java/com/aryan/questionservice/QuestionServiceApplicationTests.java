b6101d66c035b0caeaf265a0c533195b06770ab6
Aryan <aryanitinpatil@gmail.com>
2023-12-27 23:59:06 +0530

    Started With UnitTests

--- QuestionService/src/test/java/com/aryan/questionservice/QuestionServiceApplicationTests.java
+++ QuestionService/src/test/java/com/aryan/questionservice/QuestionServiceApplicationTests.java
@@ -1,13 +1,23 @@
 package com.aryan.questionservice;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.aryan.questionservice.controllers.QuestionController;
+
 
 @SpringBootTest
 class QuestionServiceApplicationTests {
 
+	@Autowired
+	private QuestionController controller;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
+	
+	
 
 }
